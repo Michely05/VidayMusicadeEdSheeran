53df534e33d4d49188af8567093c4c9b55334373
[Michely Paredes Galeano] <[michelyparedes@gmail.com]>
2024-06-22 12:21:41 +0200

    specified width and height to images on tour page

--- src/script.js
+++ src/script.js
@@ -8,25 +8,3 @@
 
   loadStyle("styles.css");
 })();
-
-document.addEventListener("DOMContentLoaded", function () {
-  var facades = document.querySelectorAll(".youtube-facade");
-  facades.forEach(function (facade) {
-    facade.addEventListener(
-      "click",
-      function () {
-        var iframe = document.createElement("iframe");
-        iframe.setAttribute(
-          "src",
-          "https://www.youtube.com/embed/" + this.dataset.embed + "?autoplay=1"
-        );
-        iframe.setAttribute("frameborder", "0");
-        iframe.setAttribute("allow", "autoplay; encrypted-media");
-        iframe.setAttribute("allowfullscreen", "");
-        this.innerHTML = "";
-        this.appendChild(iframe);
-      },
-      { passive: true }
-    );
-  });
-});
