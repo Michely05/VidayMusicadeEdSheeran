66b24e85ea6d425a5a62007c0ca1a18d3aad0d1f
[Michely Paredes Galeano] <[michelyparedes@gmail.com]>
2024-06-22 22:54:38 +0200

    used fachada to hide youtube video

--- src/script.js
+++ src/script.js
@@ -8,3 +8,25 @@
 
   loadStyle("styles.css");
 })();
+
+document.addEventListener("DOMContentLoaded", function () {
+  var placeholders = document.querySelectorAll(".youtube-placeholder");
+  placeholders.forEach(function (placeholder) {
+    placeholder.addEventListener("click", function () {
+      var embedCode = placeholder.getAttribute("data-embed");
+      var iframe = document.createElement("iframe");
+      iframe.setAttribute(
+        "src",
+        "https://www.youtube.com/embed/" + embedCode + "?autoplay=1"
+      );
+      iframe.setAttribute("frameborder", "0");
+      iframe.setAttribute(
+        "allow",
+        "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      );
+      iframe.setAttribute("allowfullscreen", "true");
+      placeholder.innerHTML = "";
+      placeholder.appendChild(iframe);
+    });
+  });
+});
